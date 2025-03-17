a56ac42edd19972169e4dac434ed9cb7c0b6c842
bfyqjzsgkqycf <19128317516@163.com>
2025-03-17 19:36:50 +0800

    feat: 添加子弹类并实现其基本功能
    添加了Bullet类，实现了子弹的初始化、绘制、飞行和碰撞检测功能。子弹的初始位置、速度和方向由构造函数参数决定。提供了两种绘制方法，一种绘制白色小圆，另一种绘制红色大圆。飞行方法更新子弹的位置，并在超出屏幕边界时重置位置。碰撞检测方法判断子弹是否与给定形状发生碰撞。

--- src/Bullet.java
+++ src/Bullet.java
@@ -1,2 +1,95 @@
+import java.awt.*;
+import java.awt.geom.Area;
+
 public class Bullet {
+    double x = 0, y = 0;
+    int speed = 9;
+    int count = 0;
+    int flagbullet = 0;
+    double speedx = 0;
+    double speedy = 0;
+    int Height = Windows.HEIGHT.getValue();
+    int Width = Windows.WIDTH.getValue();
+
+    public Bullet(double x, double y, int flag) {
+        this.x = x;
+        this.y = y;
+        this.flagbullet = flag;
+        if (flagbullet == 0) speedy -= speed;
+        else if (flagbullet == 1) {
+            speedy -= 0.866 * speed;
+            speedx += 0.5 * speed;
+        } else if (flagbullet == 2) {
+            speedy -= 0.5 * speed;
+            speedx += 0.866 * speed;
+        } else if (flagbullet == 3) speedx += speed;
+        else if (flagbullet == 4) {
+            speedy += 0.5 * speed;
+            speedx += 0.866 * speed;
+        } else if (flagbullet == 5) {
+            speedy += 0.866 * speed;
+            speedx += 0.5 * speed;
+        } else if (flagbullet == 6) speedy += speed;
+        else if (flagbullet == 7) {
+            speedy += 0.866 * speed;
+            speedx -= 0.5 * speed;
+        } else if (flagbullet == 8) {
+            speedy += 0.5 * speed;
+            speedx -= 0.866 * speed;
+        } else if (flagbullet == 9) speedx -= speed;
+        else if (flagbullet == 10) {
+            speedy -= 0.5 * speed;
+            speedx -= 0.866 * speed;
+        } else if (flagbullet == 11) {
+            speedy -= 0.866 * speed;
+            speedx -= 0.5 * speed;
+        }
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(Color.WHITE);
+        g.fillOval((int) x, (int) y, 4, 4);
+    }
+
+    public void drawred(Graphics g) {
+        g.setColor(Color.RED);
+        g.fillOval((int) x, (int) y, 10, 10);
+    }
+
+    public void fly() {
+        y += speedy;
+        x += speedx;
+        count += speed;
+        chongzhibullet();
+    }
+
+    public void setterx(int x) {
+        this.x = x;
+    }
+
+    public void settery(int y) {
+        this.y = y;
+    }
+
+    public boolean hit(Shape p) {
+        Rectangle a = new Rectangle((int) x, (int) y, 1, 1);
+        Area area = new Area(p);
+        ;
+        area.intersect(new Area(a));
+        return area.isEmpty();
+    }
+
+    private void chongzhibullet() {
+        if (x < 0) {
+            x += Width;
+        } else if (x > Width) {
+            x -= Width;
+        }
+        if (y < 0) {
+            y += Height;
+        } else if (y > Height) {
+            y -= Height;
+        }
+    }
+
 }
