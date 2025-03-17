4cc39e88920f0b07c1400917c094e43259c87a22
bfyqjzsgkqycf <19128317516@163.com>
2025-03-17 19:39:16 +0800

    feat: 将Windows类改为枚举类型并添加屏幕尺寸常量
    Windows类从public class改为public enum，添加了HEIGHT和WIDTH两个枚举常量，分别设置为1000和1600。同时添加了私有属性Value和对应的构造方法与get方法，用于存储和获取枚举常量的值。

--- src/Windows.java
+++ src/Windows.java
@@ -1,2 +1,13 @@
-public class Windows {
+public enum Windows {
+    HEIGHT(1000), WIDTH(1600);
+    private int Value;
+
+    private Windows(int rv) {
+        this.Value = rv;
+    }
+
+    public int getValue() {
+        return Value;
+    }
+
 }
