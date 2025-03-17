9f803fe3e764b91df789fc253d0fbbc5a9251404
bfyqjzsgkqycf <19128317516@163.com>
2025-03-17 19:37:45 +0800

    feat: 添加键盘和鼠标事件监听器
    添加了键盘和鼠标事件监听器，实现了玩家通过键盘控制游戏的功能。具体包括：
    - 实现了KeyListener和MouseListener接口
    - 通过按键S启动游戏
    - 通过ESC键切换菜单显示
    - 通过方向键控制玩家飞船的移动
    - 通过空格键发射子弹
    - 通过Shift键触发闪现
    - 当玩家生命值为0时，通过B键重新开始游戏并重置游戏状态

--- src/MeListener.java
+++ src/MeListener.java
@@ -1,2 +1,92 @@
-public class MeListener {
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
+import java.util.Arrays;
+
+public class MeListener implements KeyListener, MouseListener {
+    private Picture picture;
+
+    public MeListener(Picture p) {
+        picture = p;
+    }
+
+    public void mouseClicked(MouseEvent e) {
+    }
+
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    public void keyTyped(KeyEvent e) {
+    }
+
+    public void keyReleased(KeyEvent e) {
+    }
+
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_S) {
+            picture.startflag = 1;
+        }
+        if (picture.startflag == 1) {
+            if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+                picture.menuflag = (picture.menuflag + 1) % 2;
+            }
+        }
+        if (picture.startflag == 1 && picture.mehuanum > 150) {
+            if (picture.me.wudiflag != 1 && picture.menuflag == 0) {
+                if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+                    picture.me.rotateLeft();
+                } else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+                    picture.me.rotateRight();
+                } else if (e.getKeyCode() == KeyEvent.VK_UP) {
+                    picture.me.addspeed();
+                } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+                    picture.me.minusspeed();
+                } else if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+                    picture.BulletArray = Arrays.copyOf(picture.BulletArray, picture.BulletArray.length + 1);
+                    picture.BulletArray[picture.BulletArray.length - 1] = new Bullet(picture.me.x1[0] - 2, picture.me.y1[0] - 2, picture.me.flag);
+                    // 在 bulletlist 添加一个 Node
+                } else if (e.getKeyCode() == KeyEvent.VK_SHIFT) {
+                    picture.shanxian();
+                }
+                picture.repaint();
+            }
+            if (picture.life <= 0) {
+                picture.life = 0;
+                if (e.getKeyCode() == KeyEvent.VK_B) {
+                    if (picture.score > picture.highest) picture.highest = picture.score;
+                    picture.BulletArray = new Bullet[0];// 创建一个子弹数组
+                    picture.UFOArray = new UFO[0];// 创建一个 ufo数组
+                    picture.UFOBulletArray = new Bullet[0];// 创建一个 ufo子弹数组
+                    picture.asteroidsArray = new Asteroids[0];// 创建一个陨石数组
+                    picture.ballArray = new Balldisappear[0];
+                    picture.me = new Me();
+                    picture.score = 0;
+                    picture.ufobullettime = 0;
+                    picture.asteroidstime = 0;
+                    picture.ufoflag = 1;
+                    picture.wudinum = 0;
+                    picture.life = 3;
+                    picture.scoretemp = 0;
+                    picture.addasteroidsflag = 0;
+                    picture.addasteroidsnum = 1;
+                    picture.yy = -1000;
+                    picture.mehuanum = 0;
+                }
+            }
+        }
+    }
 }
