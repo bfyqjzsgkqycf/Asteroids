c21e1c6280b37ec1c3735559b51d6efe8eab4a0a
bfyqjzsgkqycf <19128317516@163.com>
2025-03-17 19:38:58 +0800

    feat: 添加主程序入口和窗口初始化
    在Window.java中添加了主程序入口方法main，初始化了JFrame窗口并设置了窗口大小、关闭操作、添加了Picture组件和键盘监听器，使窗口可见并启动了Picture组件的动作。

--- src/Window.java
+++ src/Window.java
@@ -1,2 +1,17 @@
+import javax.swing.*;
+
 public class Window {
+    public static void main(String[] args) {
+        int Height = Windows.HEIGHT.getValue();
+        int Width = Windows.WIDTH.getValue();
+        JFrame window = new JFrame();
+        window.setSize(Width, Height);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Picture pic = new Picture();
+        window.add(pic);
+        MeListener ll = new MeListener(pic);
+        window.addKeyListener(ll);
+        window.setVisible(true);
+        pic.action();
+    }
 }
