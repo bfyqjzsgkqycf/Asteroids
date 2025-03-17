76734f17b778d1fdc3c4a1a42632fd670b48197d
bfyqjzsgkqycf <19128317516@163.com>
2025-03-17 19:36:24 +0800

    feat: 添加球消失功能
    添加了球消失功能，包括球的速度、位置和计数器的初始化。球在初始化时会随机生成速度方向，并在移动时更新位置和计数器。新增了绘制球和绘制轨迹线的方法。

--- src/Balldisappear.java
+++ src/Balldisappear.java
@@ -1,2 +1,55 @@
+import java.awt.*;
+import java.awt.Graphics;
+
 public class Balldisappear {
+    double MIN_SPEED = 1;
+    double MAX_SPEED = 2;
+    double x = 0, y = 0;
+    int speed = 3;
+    int count = 0;
+    double speedy = 0;
+    double speedx = 0;
+    int flag = 0;
+    double[] xl = {20, 50};
+    double[] yl = {20, 50};
+
+    Balldisappear(double x, double y) {
+        this.speedx = MIN_SPEED + Math.random() * (MAX_SPEED - MIN_SPEED);
+        this.speedy = MIN_SPEED + Math.random() * (MAX_SPEED - MIN_SPEED);
+        this.x = x;
+        this.y = y;
+        if (Math.random() <= 0.5) {
+            speedx = -speedx;
+        }
+        if (Math.random() <= 0.5) {
+            speedy = -speedy;
+        }
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(Color.ORANGE);
+        g.fillOval((int) x, (int) y, 5, 5);
+    }
+
+    public void linepoint() {
+        for (int i = 0; i < xl.length; i++) {
+            xl[i] += x;
+            yl[i] += y;
+        }
+    }
+
+    public void drawline(Graphics g) {
+        g.setColor(Color.GREEN);
+        g.drawPolyline(new int[]{(int) xl[0], (int) xl[1]}, new int[]{(int) yl[0], (int) yl[1]}, 2);
+    }
+
+    public void fly() {
+        y += speedy;
+        x += speedx;
+        count += speed;
+        for (int i = 0; i < xl.length; i++) {
+            xl[i] += speedx;
+            yl[i] += speedy;
+        }
+    }
 }
